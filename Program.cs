d3f98564fb214770ec0a3c76e95ff623e7c79b22
Mudassar Ali <mudassar.ali@vortx.com>
2024-01-30 22:32:03 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Define a namespace called MyNamespace
+namespace MyNamespace
+{
+    // Define a class within the namespace
+    public class MyClass
+    {
+        public void PrintMessage()
+        {
+            Console.WriteLine("Hello from MyNamespace!");
+        }
+    }
+}
+
+// Main program starts here
+class Program
+{
+    static void Main()
+    {
+        // Creating an instance of MyClass from MyNamespace
+        MyNamespace.MyClass myObject = new MyNamespace.MyClass();
+
+        // Calling the method defined in MyClass
+        myObject.PrintMessage();
+    }
+}
